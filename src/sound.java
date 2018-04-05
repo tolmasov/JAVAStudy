08d1c13a373c025dbf64852af572380d081b705e
Tolmigor <tolmasov@gmail.com>
2018-04-05 19:15:27 +0300

    2-1

---
+++ src/sound.java
@@ -0,0 +1,12 @@
+/**
+ * Created by sbt-tolmasov-ia on 05.04.2018.
+ */
+public class sound {
+    public static void main(String[] args) {
+        double dist;
+
+        dist = (1100*7.2)/2;
+        System.out.println("Distance to flash "+ dist);
+
+    }
+}
--- src/test.java
+++ src/test.java
@@ -8,5 +8,33 @@ public class test {
         b=2;
         c=a+b;
         System.out.println("c теперь равно " + c);
+
+        long ci;
+        long im;
+
+        im = 5280*12;
+        ci = im*im*im;
+        System.out.println(ci);
+
+        double x,y,z;
+        x=5;
+        y=6;
+        z= Math.sqrt(x*x+y*y);
+        System.out.println(z);
+
+        char cr;
+        cr='X';
+        System.out.println("этот символ " +cr);
+        cr++;
+//        cr=90;
+        System.out.println("этот символ " +cr);
+
+
+        boolean d;
+        d=false;
+        System.out.println("d " +d);
+        d=true;
+        System.out.println("d " + (d));
+        System.out.println("10>9 "+ (9>10));
     }
 }
