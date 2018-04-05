add82b7cf917dd4bb3068af12a416f82dbb2cc24
Tolmigor <tolmasov@gmail.com>
2018-04-05 17:13:53 +0300

    my first commit

---
+++ src/test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by sbt-tolmasov-ia on 05.04.2018.
+ */
+public class test {
+    public static void main(String[] args) {
+        int a,b,c;
+        a=1;
+        b=2;
+        c=a+b;
+        System.out.println("c теперь равно " + c);
+    }
+}
